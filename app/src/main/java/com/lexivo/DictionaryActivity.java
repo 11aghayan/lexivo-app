9693b8f19a45165678b37ab9f0ebc6183c649f5a
11aghayan <11.aghayan.99@gmail.com>
2025-05-31 17:59:48 +0400

    all words activity layout done

--- app/src/main/java/com/lexivo/DictionaryActivity.java
+++ app/src/main/java/com/lexivo/DictionaryActivity.java
@@ -45,7 +45,7 @@ public class DictionaryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_dictionary);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.dictionary), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
